0f1f4a36ff0f9faf817cfaaf3fb5a9666738b6e4
Kevin McGillivray <kevin.mcgillivray@me.com>
2017-09-04 15:55:55 -0500

    Add meal choices to view

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,29 @@ const config = require('../config');
 const base = require('airtable').base(config.airtable.baseId);
 
 router.get('/', function(req, res) {
-  res.render('index');
+  base('Meals').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 5,
+    view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+      console.log(record);
+    });
+
+    res.render('index', {
+      meals: records
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+  }, function done(err) {
+    if (err) { console.error(err); return; }
+  });
 });
 
 // capture raw input and save it to the database as a single record
